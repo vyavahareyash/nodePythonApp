f8d1e8b3eaaa0f43b8a824d028e25decb1726c1c
Yash Vyavahare <94339743+vyavahareyash@users.noreply.github.com>
2023-04-05 01:42:47 +0530

    remove python

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+var request = require('request');
 const { spawn } = require('child_process');
 const bodyParser = require('body-parser')
 const app = express()
@@ -14,24 +15,34 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + "/signup.html");
 })
 
+// app.post('/', function(req, res) {
+
+//     const number = req.body.number;
+
+//     var dataToSend;
+//     // spawn new child process to call the python script
+//     const python = spawn('python', ['public/script1.py', number]);
+//     // collect data from script
+//     python.stdout.on('data', function(data) {
+//         console.log('Pipe data from python script ...');
+//         dataToSend = data.toString();
+//     });
+//     // in close event we are sure that stream from child process is closed
+//     python.on('close', (code) => {
+//         console.log(`child process close all stdio with code ${code}`);
+//         // send data to browser
+//         res.render('result', { dataToSend: dataToSend })
+//     });
+// })
+
 app.post('/', function(req, res) {
-
-    const number = req.body.number;
-
-    var dataToSend;
-    // spawn new child process to call the python script
-    const python = spawn('python', ['public/script1.py', number]);
-    // collect data from script
-    python.stdout.on('data', function(data) {
-        console.log('Pipe data from python script ...');
-        dataToSend = data.toString();
-    });
-    // in close event we are sure that stream from child process is closed
-    python.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        // send data to browser
+    const number = parseInt(req.body.number);
+    // console.log(number)
+    request.post("https://yashvyavahare.ap-south-1.modelbit.com/v1/example_doubler/latest", { json: { data: number } }, function(request, response) {
+        dataToSend = response.body.data;
         res.render('result', { dataToSend: dataToSend })
-    });
+    })
+
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
