3016a1e5b05fe3f34671924549af68ecc1111d0d
Yash Vyavahare <94339743+vyavahareyash@users.noreply.github.com>
2023-04-12 23:45:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 const fileUpload = require('express-fileupload');
 const app = express();
+const path = require("path");
 
 app.use(fileUpload());
 require('dotenv').config()
@@ -22,14 +23,14 @@ const secretAccessKey = 'hpshLhSVZoJM4ZtBmzNbNRuwpYjOsBteTpBjZbrt'
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
 
+const s3 = new S3({
+    region,
+    accessKeyId,
+    secretAccessKey
+})
 app.post('/imageUpload', async(req, res) => {
 
 
-    const s3 = new S3({
-        region,
-        accessKeyId,
-        secretAccessKey
-    })
 
     function uploadFile(data) {
 
@@ -49,10 +50,46 @@ app.post('/imageUpload', async(req, res) => {
     // Setting up S3 upload parameters
 
     // Uploading files to the bucket
-    uploadFile(fileContent, filename).then(() => res.send('ok'));
+    uploadFile(fileContent, filename).then(() => res.redirect('/show'));
+});
 
+app.get('/show', async(req, res) => {
+    // res.send("showing");
+    var item = req.body;
+    var params = { Bucket: bucketName, Key: 'cat.png' }; // keyname can be a filename
+    // s3.getObject(params, function(err, data) {
+    //     if (err) {
+    //         return res.send({ "error": err });
+    //     }
+    //     res.send({ data });
+    // });
+    async function getImage() {
+        const data = s3
+            .getObject({
+                Bucket: bucketName,
+                Key: "cat.png",
+            })
+            .promise();
+        return data;
+    }
 
-
+    function encode(data) {
+        let buf = Buffer.from(data);
+        let base64 = buf.toString("base64");
+        return base64;
+    }
+    getImage()
+        .then((img) => {
+            let image =
+                "<img src='data:image/jpeg;base64," + encode(img.Body) + "'" + "/>";
+            let startHTML = "<html><body></body>";
+            let endHTML = "</body></html>";
+            let html = startHTML + image + endHTML;
+            res.send(html);
+        })
+        .catch((e) => {
+            res.send(e);
+        });
 })
 
 app.listen(3000, function() {
