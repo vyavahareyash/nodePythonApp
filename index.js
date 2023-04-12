268448cdea1df8124e88cff41a3d4585707ee5fb
Yash Vyavahare <94339743+vyavahareyash@users.noreply.github.com>
2023-04-12 23:14:01 +0530

    s3

--- index.js
+++ index.js
@@ -1,117 +1,58 @@
-const express = require('express');
-// const path = require('path')
-var request = require('request');
-// const { spawn } = require('child_process');
-const bodyParser = require('body-parser');
-// const fileUpload = require('express-fileupload');
-const mongoose = require('mongoose');
-const url = 'mongodb+srv://yash:yash@cluster0.kdb13b7.mongodb.net';
-const dbName = 'deploymentModels';
-const multer = require('multer');
-const ImageModel = require("./image.model");
+var express = require('express')
+const fileUpload = require('express-fileupload');
 const app = express();
-const port = 3000;
 
-mongoose.connect(url + '/' + dbName, { useNewUrlParser: true }).then(() => console.log("DB is connected.")).catch((err) => console.log(err, "it has an error"));
-
-// const imageSchema = new mongoose.Schema({
-//     id: Number,
-//     data: String,
-// });
-
-// const Image = mongoose.model('Image', imageSchema);
-
-// app.use(fileUpload());
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-const Storage = multer.diskStorage({
-    destination: 'uploadedImages',
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    }
-});
-
-const upload = multer({
-    storage: Storage
-});
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/signup.html");
-})
-
-app.get('/image', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
+app.use(fileUpload());
+require('dotenv').config()
+const fs = require('fs')
+const S3 = require('aws-sdk/clients/s3')
 
 
-app.post('/upload', upload.single('yash'), (req, res) => {
-    // Log the files to the console
-    // const data = JSON.stringify(req.files.image.data);
-    // res.send(data);
-    // console.log(data);
+// var AWS = require('aws-sdk');
 
-    // const image = new Image({
-    // id: 55,
-    // data: data
-    // });
+// const bucketName = process.env.AWS_BUCKET_NAME
+// const region = process.env.AWS_BUCKET_REGION
+// const accessKeyId = process.env.AWS_ACCESS_KEY
+// const secretAccessKey = process.env.AWS_SECRET_KEY
 
-    // image.save()
+const bucketName = 'my-bucket-yash-788921312'
+const region = 'us-east-1'
+const accessKeyId = 'AKIAQWOBXYSMW5DKHGNS'
+const secretAccessKey = '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'
 
-    // All good
-    // res.sendStatus(200);
-    ///////////////////////////////////////////////////////////////////
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
 
-    // upload(req, res, (err) => {
-    //     // res.send(req.body.name);
-    //     console.log(req.body);
-    //     if (err) {
-    //         console.log("ERROR:\n" + err)
-    //     } else {
-    const newImage = new ImageModel({
-        name: req.file.originalname,
-        image: {
-            data: req.file.filename,
-            contentType: 'image'
-        }
-    });
-    newImage.save()
-        .then(() => res.send('successfully uploaded image'))
-        .catch((err) => console.log(err));
-    //     }
-    // })
-});
+app.post('/imageUpload', async(req, res) => {
 
-// app.post('/', function(req, res) {
 
-//     const number = req.body.number;
-
-//     var dataToSend;
-//     // spawn new child process to call the python script
-//     const python = spawn('python', ['public/script1.py', number]);
-//     // collect data from script
-//     python.stdout.on('data', function(data) {
-//         console.log('Pipe data from python script ...');
-//         dataToSend = data.toString();
-//     });
-//     // in close event we are sure that stream from child process is closed
-//     python.on('close', (code) => {
-//         console.log(`child process close all stdio with code ${code}`);
-//         // send data to browser
-//         res.render('result', { dataToSend: dataToSend })
-//     });
-// })
-
-app.post('/', function(req, res) {
-    const number = parseInt(req.body.number);
-    // console.log(number)
-    request.post("https://yashvyavahare.ap-south-1.modelbit.com/v1/example_doubler/latest", { json: { data: number } }, function(request, response) {
-        dataToSend = response.body.data;
-        res.render('result', { dataToSend: dataToSend })
+    const s3 = new S3({
+        region,
+        accessKeyId,
+        secretAccessKey
     })
 
+    function uploadFile(data) {
+
+        const uploadParams = {
+            Bucket: bucketName,
+            Body: fileContent,
+            Key: 'finaltest.png'
+        }
+
+        return s3.upload(uploadParams).promise()
+    }
+
+    // Binary data base64
+    const fileContent = Buffer.from(req.files.image.data, 'binary');
+
+    // Setting up S3 upload parameters
+
+    // Uploading files to the bucket
+    uploadFile(fileContent).then(() => console.log('done'));
+
+
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
